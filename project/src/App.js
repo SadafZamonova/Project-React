8c6db7bb434e13ae2bfa073145b4682a5eab17e9
SadafZamonova <84777664+SadafZamonova@users.noreply.github.com>
2021-12-21 02:44:54 +0500

    install  bootstrap

---
+++ project/src/App.js
@@ -0,0 +1,18 @@
+import * as ReactBootStrap from 'react-bootstrap';
+import './App.css';
+import Footer from './components/footer';
+
+function App() {
+  return (
+    <div className="page-container">
+      <div className="content-wrap">
+       
+      
+      </div>
+      <Footer/>
+    </div>
+  );
+}
+
+
+export default App;
