20cd9554bbe388fde1705df5fdf4c6020a6343cd
SadafZamonova <84777664+SadafZamonova@users.noreply.github.com>
2022-02-16 15:49:39 +0500

    3

--- project/src/components/main.jsx
+++ project/src/components/main.jsx
@@ -1,4 +1,6 @@
 import WordList from "./wordList";
+import data from './data' 
+import CardList from "./cardList";
 
 
 
@@ -7,9 +9,10 @@ const Main = () => {
     return ( 
         <>
 <h2>Список слов</h2>
-<WordList/>
+<WordList data = {data}/>
+<CardList data = {data}/>
 </>   
 );
 }
     
-export default Main;
+export default Main; 
