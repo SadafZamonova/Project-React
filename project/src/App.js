d7fb839598f507d358ee7213e1b8c438381532e1
SadafZamonova <84777664+SadafZamonova@users.noreply.github.com>
2022-02-24 12:12:06 +0500

    роутинг

--- project/src/App.js
+++ project/src/App.js
@@ -3,15 +3,19 @@ import './App.css';
 import Main from './components/main';
 import Header from './components/header';
 //import WordList from './components/wordList';
+import { BrowserRouter as Router,
+ } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
   <div className='list_wrap'>
      <Header/>
       <Main/>
 
       
     </div>
+    </Router>
   )
 }
 
